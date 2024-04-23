486546f8c88fffebd0115e77e5ca8b8dee92b804
Haoyuan Geng <steven.h.geng@outlook.com>
2024-04-22 23:52:22 -0400

    finished Docker setup

---
+++ frontend/amazon/runserver.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Running as user: $(whoami)"
+
+python3 manage.py makemigrations || echo "makemigrations failed with exit code $?"
+
+python3 manage.py migrate || echo "migrate failed with exit code $?"
+
+# if [ -d "./product_image" ]; then
+#     chmod -R 775 ./product_image
+# else
+#     echo "Directory not found: ./product_image"
+# fi
+
+python3 manage.py loaddata initial_data_amazon.json || echo "loaddata failed with exit code $?"
+
+#echo "Starting server..."
+
+python3 manage.py runserver 0.0.0.0:8080 || echo "Failed to start server $?"
