1cf8c73dd5ad47a1795fe39e8c491cfd5c47fec7
Haoyuan Geng <steven.h.geng@outlook.com>
2024-04-25 16:39:28 -0400

    refactory

--- backend/app/src/main/java/backend/utils/Sender.java
+++ backend/app/src/main/java/backend/utils/Sender.java
@@ -1,58 +1,40 @@
 package backend.utils;
 
+import java.io.IOException;
+import java.io.OutputStream;
+
 import com.google.protobuf.CodedOutputStream;
 import com.google.protobuf.GeneratedMessageV3;
 
-import java.io.*;
-
 public class Sender {
-    /**
-     * This function will "send" the data(should follow Google Protocol Buffer) to corresponding output stream.
-     * You should handle the timeout by yourself.
-     * @param msg the message to be send
-     * @param out output stream
-     * @param <T> generic type of the data
-     * @return send result
-     */
     public static <T extends GeneratedMessageV3> boolean sendMsgTo(T msg, OutputStream out) {
         try {
-            byte[] data = msg.toByteArray();
-            CodedOutputStream codedOutputStream = CodedOutputStream.newInstance(out);
-            codedOutputStream.writeUInt32NoTag(data.length);
-            codedOutputStream.writeRawBytes(data);
-            // NOTE!!! always flush the result to stream
-            codedOutputStream.flush();
+            byte[] msgBytes = msg.toByteArray();
+            //sendBytes(msgBytes, out);
+            CodedOutputStream COS = CodedOutputStream.newInstance(out);
+            COS.writeUInt32NoTag(msgBytes.length);
+            COS.writeRawBytes(msgBytes);
+            COS.flush();
             return true;
         } catch (IOException e) {
-            System.err.println("sendToWorld: " + e.toString());
+            System.err.println(e.toString());
             return false;
         }
     }
 
-    // public static <T extends GeneratedMessageV3> boolean sendMessage(T message, OutputStream outputStream) {
+    // private static void sendBytes(byte[] data, OutputStream out) throws IOException {
     //     try {
-    //         byte[] data = message.toByteArray();
-    //         CodedOutputStream codedOutputStream = CodedOutputStream.newInstance(outputStream);
-    //         writeMessageLength(codedOutputStream, data.length);
-    //         writeMessageData(codedOutputStream, data);
-    //         codedOutputStream.flush();
-    //         return true;
-    //     } catch (IOException e) {
-    //         logError("Failed to send message", e);
-    //         return false;
+    //         out.write(data.length >> 24 & 0xFF);
+    //         out.write(data.length >> 16 & 0xFF);
+    //         out.write(data.length >> 8 & 0xFF);
+    //         out.write(data.length & 0xFF);
+    //         out.write(data);
+    //         out.flush();
+    //     } finally {
+    //         if (out != null) {
+    //             out.close();
+    //         }
     //     }
-    // }
-    
-    // private static void writeMessageLength(CodedOutputStream outputStream, int length) throws IOException {
-    //     outputStream.writeUInt32NoTag(length);
-    // }
-    
-    // private static void writeMessageData(CodedOutputStream outputStream, byte[] data) throws IOException {
-    //     outputStream.writeRawBytes(data);
-    // }
-    
-    // private static void logError(String message, IOException exception) {
-    //     System.err.println(message + ": " + exception.toString());
     // }
     
 }
